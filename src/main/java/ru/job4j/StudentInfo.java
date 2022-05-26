298ed05613260ee319e4863bc6e0ea4c4d1f02d7
Bazil82 <program1408@gmail.com>
2022-05-26 19:23:02 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.util.Arrays;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vasily Kozlov");
+        System.out.println("04.12.1982");
+    }
+}
