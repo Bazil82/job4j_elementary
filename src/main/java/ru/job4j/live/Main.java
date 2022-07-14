3f882456f50c13eeae0a50d4e8fd280212876303
Bazil82 <program1408@gmail.com>
2022-07-14 11:55:58 +0300

    5.9.1. Генерация конструкций в IDEA [#279134 #321678] ver.2

--- src/main/java/ru/job4j/live/Main.java
+++ src/main/java/ru/job4j/live/Main.java
@@ -2,8 +2,8 @@ package ru.job4j.live;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("public static void main");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
         }
     }
 }
