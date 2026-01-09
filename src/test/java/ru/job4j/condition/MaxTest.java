bdbe7cfe3e8aaa907ccc49c25edcc7fb57637262
CUSTOM <program1408@gmail.com>
2026-01-09 19:56:18 +0300

    3. Перегрузить метод max для трех чисел [#122644 #328455]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,35 +1,41 @@
 package ru.job4j.condition;
 
-import org.junit.jupiter.api.Test;
 import org.junit.Assert;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
 class MaxTest {
 
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int res = Max.max(1, 2);
+        int first = 1;
+        int second = 2;
+        Max maximum = new Max();
+        int res = maximum.max(first, second);
         int exp = 2;
         Assert.assertEquals(res, exp);
     }
 
     @Test
-    public void whenMax2To1Then2() {
-        int left = 2;
-        int right = 1;
-        int res = Max.max(2, 1);
-        int exp = 2;
+    public void whenMax1To3To2Then3() {
+        int first = 1;
+        int second = 3;
+        int third = 2;
+        Max maximum = new Max();
+        int res = maximum.max(first, second, third);
+        int exp = 3;
+
         Assert.assertEquals(res, exp);
     }
 
     @Test
-    public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int res = Max.max(2, 2);
-        int exp = 2;
+    public void whenMax10To7To2To9Then10() {
+        int first = 10;
+        int second = 7;
+        int third = 2;
+        int fourth = 9;
+        Max maximum = new Max();
+        int res = maximum.max(first, second, third, fourth);
+        int exp = 10;
         Assert.assertEquals(res, exp);
     }
 }
