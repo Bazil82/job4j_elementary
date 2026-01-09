a65a323bfcf13084d6cfb8c9d85c7bde1df9356e
CUSTOM <program1408@gmail.com>
2026-01-09 19:11:15 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587 #328372]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,41 +1,34 @@
 package ru.job4j.condition;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 class PointTest {
 
     @Test
     void when00to20then2() {
-        double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        double exp = 2;
+        Point pointTwo = new Point(2, 0);
+        Point pointOne = new Point(0, 0);
+        double out = pointTwo.distance(pointOne);
+        Assert.assertEquals(exp, out, 0.01);
     }
 
     @Test
     public void when310to56then4() {
         double exp = 4.47;
-        int x1 = 3;
-        int y1 = 10;
-        int x2 = 5;
-        int y2 = 6;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(exp, withPrecision(0.01));
+        Point pointTwo = new Point(5, 6);
+        Point pointOne = new Point(3, 10);
+        double out = pointTwo.distance(pointOne);
+        Assert.assertEquals(exp, out, 0.01);
     }
 
     @Test
     public void when07to00then7() {
         double exp = 7;
-        int x1 = 0;
-        int y1 = 7;
-        int x2 = 0;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(exp, withPrecision(0.01));
+        Point pointTwo = new Point(0, 0);
+        Point pointOne = new Point(0, 7);
+        double out = pointTwo.distance(pointOne);
+        Assert.assertEquals(exp, out, 0.01);
     }
 }
